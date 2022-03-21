80a69fc821ff6cef86ed2747352bb1224e836d56
derodero24 <k.itsh20524abc@gmail.com>
2022-03-21 23:06:54 +0900

    add onGetNftOwner() onCreateSplToken()

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,110 @@
 import { useEffect } from 'react';
 
-import * as anchor from '@project-serum/anchor';
+import * as spl from '@solana/spl-token';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import * as web3 from '@solana/web3.js';
 
 export default function App(): JSX.Element {
-  const generatePublickeyFromBs58 = (base58Address: string): web3.PublicKey => {
-    const pubkeyBuffer = anchor.utils.bytes.bs58.decode(base58Address);
-    const generatedPubkey = new web3.PublicKey(pubkeyBuffer);
-    return generatedPubkey;
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  const getNftOwner = (mintAddress: string): Promise<string> => {
+    return connection
+      .getTokenLargestAccounts(new web3.PublicKey(mintAddress))
+      .then(res => {
+        const tokenAccount = res.value[0].address;
+        return connection.getParsedAccountInfo(tokenAccount);
+      })
+      .then(res => {
+        const data = res.value?.data as web3.ParsedAccountData;
+        const ownerAddress = data.parsed.info.owner;
+        return ownerAddress;
+      });
   };
 
-  useEffect(() => {
-    // How to use
-    const address = '3ME9Uv8NaWG5cwPnTtoSxzy9haaFAxKcVjpCLRUiDyod'; // string
-    const pubkey = generatePublickeyFromBs58(address); // web3.PublicKey
-    console.log(pubkey.toBase58());
-  }, []);
+  const createSplToken = async (decimals: number, amount: number) => {
+    if (!wallet.publicKey) {
+      console.log('Please connect a wallet before.');
+      return;
+    }
 
-  return <div>Hello World</div>;
+    // Generate/Get keys
+    const mint = web3.Keypair.generate();
+    const associatedTokenAddress = await spl.getAssociatedTokenAddress(
+      mint.publicKey,
+      wallet.publicKey
+    );
+
+    // Transaction for creating NFT
+    const transaction = new web3.Transaction().add(
+      // create mint account
+      web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: spl.MINT_SIZE,
+        lamports: await spl.getMinimumBalanceForRentExemptMint(connection),
+        programId: spl.TOKEN_PROGRAM_ID,
+      }),
+      // init mint account
+      spl.createInitializeMintInstruction(
+        mint.publicKey, // mint pubkey
+        decimals, // decimals
+        wallet.publicKey, // mint authority
+        wallet.publicKey // freeze authority
+      ),
+      // create associated-token-account
+      spl.createAssociatedTokenAccountInstruction(
+        wallet.publicKey, // payer
+        associatedTokenAddress, // associated-token-address
+        wallet.publicKey, // owner
+        mint.publicKey // mint
+      ),
+      // mint to
+      spl.createMintToInstruction(
+        mint.publicKey, // mint
+        associatedTokenAddress, // receiver
+        wallet.publicKey, // mint authority
+        amount // amount
+      )
+    );
+
+    // Transaction実行
+    console.log('sending a transaction...');
+    const signature = await wallet.sendTransaction(transaction, connection, {
+      signers: [mint],
+    });
+    await connection.confirmTransaction(signature, 'processed');
+    console.log('successfully create SPL tokens');
+  };
+
+  const onGetNftOwner = async () => {
+    // create and mint 100 new tokens
+    // Owner of "Degen Ape #1506" (only at Mainnet-beta)
+    const address = 'HgDxaNFdq5HExHm2am6UfvVeiCTWdT6J5UzVu4NQR9MN';
+    getNftOwner(address).then(res => console.log('owner:', res));
+  };
+
+  const onCreateSplToken = async () => {
+    // create and mint 100 new tokens
+    const decimals = 9;
+    const amount = 100 * 10 ** decimals;
+    await createSplToken(decimals, amount);
+  };
+
+  if (!wallet.publicKey) {
+    return (
+      <>
+        <WalletMultiButton />
+        <WalletDisconnectButton />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <button onClick={onGetNftOwner}>Get Owner Test</button>
+        <button onClick={onCreateSplToken}>Create SPL Token</button>
+      </>
+    );
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
 import App from './App';
+
+// const network = clusterApiUrl('mainnet-beta'); // devnet/testnet/mainnet-beta
+const network = clusterApiUrl('devnet'); // devnet/testnet/mainnet-beta
+const wallets = [new PhantomWalletAdapter()]; // 対応ウォレット
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint={network}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
